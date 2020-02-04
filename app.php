af9db7b66061b3e2860b05ac214c1d777137421d
Andrey Semka <k9nz00@yandex.ru>
2020-02-05 00:25:01 +0300

    done task1

--- app.php
+++ app.php
@@ -1,6 +1,9 @@
 <?php
 
+use Symfony\Component\Console\Application;
+
 require_once  __DIR__ . DIRECTORY_SEPARATOR. 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-$app = new \Symfony\Component\Console\Application('task4');
+$app = new Application('task4');
+$app->add(new \App\GetWordCommand());
 $app->run();
---
+++ src/GetWordCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GetWordCommand extends Command
+{
+    protected static $defaultName = 'command_1';
+
+    protected function configure()
+    {
+        $this->setName('command_1');
+        $this->setDescription('Выполнение команды 1');
+        $this->setHelp('Команда выводит входной параметр на экран');
+        $this->addArgument('Param', InputArgument::REQUIRED, 'Переменная для вывода', null);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln($input->getArgument('Param'));
+        return 1;
+    }
+}
